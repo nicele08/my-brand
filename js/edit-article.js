94d6de9d3cb0428f7ac669de74306957f9ff22e0
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-11-10 09:58:58 +0200

    feat(blog page): validate forms \- validate edit article page \- validate add article page [Finish]

--- js/edit-article.js
+++ js/edit-article.js
@@ -1,6 +1,7 @@
 
 import {validateArticleAuthor, validateArticleContent, validateArticleImage, validateArticleName, validateArticleVisible } from "./functions/validateArticleForm.js";
 const saveBtn = document.getElementById("save-button");
+const deleteBtn = document.getElementById("delete-button");
 const err = document.getElementsByClassName("error");
 const articleTitle = document.getElementById("article-title");
 const articleVisible = document.getElementById("visible");
@@ -9,6 +10,12 @@ const articleContent = document.getElementById("content");
 const articleAuthor = document.getElementById("author");
 const modal = document.getElementById("pop-modal");
 const title = document.getElementById("title");
+const firstMessage = document.getElementById("modal-message");
+const secondMessage = document.getElementById("modal-message-two");
+const closeModal = document.getElementById("modal-cancel");
+const continueModal = document.getElementById("modal-continue");
+
+title.textContent = articleTitle.value;
 
 saveBtn.onclick = function(){
     const nameValid = validateArticleName(articleTitle, err);
@@ -17,14 +24,26 @@ saveBtn.onclick = function(){
     const contentValid = validateArticleContent(articleContent, err);
     const authorValid = validateArticleAuthor(articleAuthor, err);
     
+    
     if(nameValid == true &&
          imageValid == true &&
          contentValid == true &&
          authorValid == true){
-             title.textContent = articleTitle.value;
+             firstMessage.style.display = "none";
+             continueModal.style.display = "none";
+             secondMessage.textContent = "Article has been successful updated";
              modal.style.display = "block";
     }
 
 
+    return false;
+}
+
+deleteBtn.onclick = function(){
+    secondMessage.textContent = "Are you sure to delete?";
+    closeModal.textContent = "Cancel";
+    firstMessage.style.display = "block";
+    continueModal.style.display = "block";
+    modal.style.display = "block";
     return false;
 }
--- js/functions/validateArticleForm.js
+++ js/functions/validateArticleForm.js
@@ -1,39 +1,81 @@
-import { addShadow, removeShadow } from "./shadows.js"
-
-let isValid = false;
+import { addShadow, removeShadow } from "./shadows.js";
 
 export function validateArticleName(articleName, err){
-    if(articleName.value.length < 1){
-        err.textContent = "Title should have something.";
+    let isValid = false;
+    if(articleName.value == null || articleName.value.length < 1){
+        err[0].textContent = "*Title should have something.";
+        err[0].style.display = "block";
         addShadow(articleName, "2px 2px 2px red");
     }else{
         removeShadow(articleName);
+        err[0].style.display = "none";
         isValid = true;
     }
+
+    return isValid;
 }
 
 export function validateArticleVisible(articleVisible, err){
     if(articleVisible.checked === false){
-        err.textContent = "Article will not visible to the public";
+        err[1].textContent = "Article will not visible to the public";
+        err[1].style.display = "block";
         addShadow(articleVisible, "2px 2px 2px red");
     }else{
-        removeShadow(articleName);
-        isValid = true;
+        removeShadow(articleVisible);
+        err[1].style.display = "none";
     }
 }
 
 export function validateArticleImage(articleImage, err){
+    let isValid = false;
     const allowedExtensions = ["png", "jpg", "jpeg", "gif"];
-    const img = articleImage.files[0].name;
-    const imgExtension = img.substring(img.lastIndexOf("."));
-
-    if(allowedExtensions.includes(imgExtension.toLowerCase()) != true){
-        err.textContent = "Image should be in .png, .jpg, .jpeg and .gif formats";
-        addShadow(articleVisible, "2px 2px 2px red");
+    
+    if(articleImage.files.length === 0){
+        err[2].textContent = "*Image is required";
+        err[2].style.display = "block";
+        addShadow(articleImage, "2px 2px 2px red");
     }else{
-        removeShadow(articleName);
-        isValid = true;
+        err[2].style.display = "none";
+        const img = articleImage.files[0].name;
+        const imgExtension = img.substring(img.lastIndexOf(".")+1);
+        if(allowedExtensions.includes(imgExtension.toLowerCase()) != true){
+            err[2].textContent = "*Image should be in .png, .jpg, .jpeg and .gif formats";
+            err[2].style.display = "block";
+            addShadow(articleImage, "2px 2px 2px red");
+        }else{
+            err[2].style.display = "none";
+            removeShadow(articleImage);            
+            isValid = true;
+        }
     }
+
+    return isValid;
 }
 
-export default isValid
+export function validateArticleContent(articleContent, err){
+    let isValid = false;
+    if(articleContent.value == null || articleContent.value.length < 200){
+        err[4].textContent = "*Article should have at least 50 words.";
+        err[4].style.display = "block";
+        addShadow(articleContent, "2px 2px 2px red");
+    }else{
+        err[4].style.display = "none";
+        removeShadow(articleContent);
+        isValid = true;
+    }
+    return isValid;
+}
+
+export function validateArticleAuthor(articleAuthor, err){
+    let isValid = false;
+    if(articleAuthor.value === ""){
+        err[3].textContent = "*User should be selected.";
+        err[3].style.display = "block";
+        addShadow(articleAuthor, "2px 2px 2px red");
+    }else{
+        err[3].style.display = "none";
+        removeShadow(articleAuthor);
+        isValid = true;
+    }
+    return isValid;
+}
