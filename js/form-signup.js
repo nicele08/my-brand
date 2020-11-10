a2238e14238206ce43b84288c9977066a9c2e7ff
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-11-10 21:40:22 +0200

    feat(signup page): update signup page \- validate signup form \- add logo [Finish]

--- js/form-signup.js
+++ js/form-signup.js
@@ -1,10 +1,8 @@
-import validateName from "./functions/validateName.js";
 import validateEmail from "./functions/validateEmail.js";
 import { passwordMatch, validatePassword } from "./functions/validatePassword.js";
 
-const name = document.getElementsByName("name");
+
 const email = document.getElementsByName("email")[0];
-const message = document.getElementsByName("password")[0];
 const submit = document.getElementById("send-button");
 const err = document.getElementsByClassName("error");
 const password = document.getElementsByName("password");
@@ -12,27 +10,14 @@ const password = document.getElementsByName("password");
 submit.onclick = function(){
     let passValidation = true;
 
-    if(!validateName(name[0], err[0], "first ")){
+    if(!validateEmail(email, err[0])){
+        passValidation = false;
+    }
+    if(!validatePassword(password[0], err[1])){
         passValidation = false;
     }
 
-    if(!validateName(name[1], err[1], "last ")){
-        passValidation = false;
-    } 
-
-    if(!validateEmail(email, err[2])){
-        passValidation = false;
-    }
-    
-    if(!validateName(name[2], err[3], "user")){
-        passValidation = false;
-    }
-
-    if(!validatePassword(password[0], err[4])){
-        passValidation = false;
-    }
-
-    if(!passwordMatch(password[0], password[1], err[5])){
+    if(!passwordMatch(password[0], password[1], err[2])){
         passValidation = false;
     }
 
--- js/functions/validatePassword.js
+++ js/functions/validatePassword.js
@@ -3,7 +3,7 @@ import { addShadow, removeShadow } from "./shadows.js";
 export function validatePassword(element, err){
     const regex = /\d/g;
     if(element.value.length < 8 || !regex.test(element.value)){
-        err.textContent = "Password should be atleast 8 characters with numbers"
+        err.textContent = "Password should be atleast 8 characters with numbers";
         addShadow(element, "2px 2px 2px red");
         return false;
     }else{
@@ -15,7 +15,7 @@ export function validatePassword(element, err){
 
 export function passwordMatch(element1, element2, err){
     if(element1.value !== element2.value){
-        err.textContent = "Password not match"
+        err.textContent = "Password not match";
         addShadow(element2, "2px 2px 2px red");
         return false;
     }else{
