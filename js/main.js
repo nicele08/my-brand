6a1b8247be03202e817d876d9f630885f530b1bc
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-11-13 17:37:58 +0200

    feat(blog): add logo \- add logo.js [Finish]

--- js/main.js
+++ js/main.js
@@ -29,34 +29,6 @@ function changeMenu(id){
     menuIconOpen ? closeNav() : openNav();
 }
 
-let canvas = document.getElementById("logo-blog");
-let ctx = canvas.getContext("2d");
-
-let grd = ctx.createRadialGradient(canvas.width/2, canvas.height/2, 20, canvas.width/2, canvas.height/2, 70);
-grd.addColorStop(0, "#5C4978");
-grd.addColorStop(0.6, "#5C4978");
-ctx.fillStyle = grd;
-ctx.fillRect(0,0,100,70);
-
-ctx.font = "italic small-caps bold 32px sans-serif";
-ctx.fillStyle = "#CCFFFF";
-ctx.textAlign = "center";
-ctx.fillText("NICE", canvas.width/2, 24);
-
-ctx.beginPath();
-ctx.moveTo(14, 27);
-ctx.quadraticCurveTo(10, 45, 75, 28);
-
-let gradient = ctx.createLinearGradient(0, 0, 170, 0);
-gradient.addColorStop("0", "magenta");
-gradient.addColorStop("0.5" ,"#CCFFFF");
-gradient.addColorStop("1.0", "red");
-
-// Fill with gradient
-ctx.strokeStyle = gradient;
-ctx.lineWidth = 5;
-ctx.stroke();
-
 let modal = document.getElementById("pop-modal");
 let spanClose = document.getElementsByClassName("close")[0];
 let cancelBtn = document.getElementById("modal-cancel");
