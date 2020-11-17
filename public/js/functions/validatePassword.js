22a08aac1a6408fdd8eba5b8fbcbc0da8938d1dc
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-11-17 15:11:54 +0200

    feat(signup page): signup with email and password \- authenticate user by email and password \- add social media links [Finish]

---
+++ public/js/functions/validatePassword.js
@@ -0,0 +1,26 @@
+import { addShadow, removeShadow } from "./shadows.js";
+
+export function validatePassword(element, err){
+    const regex = /\d/g;
+    if(element.value.length < 8 || !regex.test(element.value)){
+        err.textContent = "Password should be atleast 8 characters with numbers";
+        addShadow(element, "2px 2px 2px red");
+        return false;
+    }else{
+        err.textContent = "";
+        removeShadow(element);
+        return true;
+    }
+}
+
+export function passwordMatch(element1, element2, err){
+    if(element1.value !== element2.value){
+        err.textContent = "Password not match";
+        addShadow(element2, "2px 2px 2px red");
+        return false;
+    }else{
+        err.textContent = "";
+        removeShadow(element2);
+        return true;
+    }
+}
