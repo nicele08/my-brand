7f471e28668982ee857094a4185ef0d75c55e527
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-11-19 09:42:21 +0200

    feat(article firebase): add and retrieve article \- add article to firebase \- retrieve article from firebase \- delete article [Finish]

---
+++ public/js/functions/updateCount.js
@@ -0,0 +1,10 @@
+export default function updateCount(checkboxes){
+    let count = 0;
+    for(let i=0; i<checkboxes.length; i++){
+        if(checkboxes[i].checked === true){
+            checkboxes[i].classList.add("article-selected");
+            count++;
+        }
+    }
+    return count;
+}
