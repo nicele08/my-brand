a0d44d37d896d039ee0a8fbcd7776e8f45883488
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-12-01 18:41:10 +0200

    feat(user account api): create account api
    \- validate signup
    \- validate login
    [Finish]

---
+++ server/models/userModel.js
@@ -0,0 +1,15 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = Schema({
+    _id: Schema.Types.ObjectId,
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    },
+    password: {type: String, required: true},
+    userType: {type: String, default: "client"}
+});
+
+export default model('User', userSchema);
