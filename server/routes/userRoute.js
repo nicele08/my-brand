d2f72135c2d9e26ad5b01bc30bd6805ff88cdf4b
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-12-07 18:54:59 +0200

    feat(query api): test routes with mocha \- test post route to store query \- test get route to get queries \- test patch route to update query \- test delete route to delete query [Finish]

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {userSignup, userLogin} from "../controllers/userController";
-
-const userRoutes = express.Router();
-
-userRoutes.post("/signup", userSignup);
-userRoutes.post("/login", userLogin);
-
-export default userRoutes;
