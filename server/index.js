99518c95970bd75e14a90d703cd792c921308b96
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2020-12-10 16:52:19 +0200

    feat(article api): solve conflicts \- solve conflicts with .gitignore \- solve coflicts with index.js \- solve conflicts with .test.js [Done]

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import userRoutes from './routes/userRoute';
+import mongoose from './db/mongoose';
 
 const app = express();
+
+const db = mongoose;
+app.use('/database', (req, res) => {
+  res.status(500).json({
+    dbState: db.connection.readyState,
+  });
+});
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -15,12 +26,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', (req, res) => {
+app.use('/home', (req, res) => {
   res.status(200)
     .json({
       status: 'Active',
     });
 });
+
+app.use('/users', userRoutes);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
